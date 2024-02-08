6c22442860d03cfd9d431cdb73c0cf650cdfebb2
UthfolGhosh <uthfolghosh9038@gmail.com>
2024-02-09 01:00:09 +0600

    update bfs code

--- BFS.cpp
+++ BFS.cpp
@@ -1,53 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int>adj_list[30000];
+
+void BFS(int node)
+{
+   vector<bool> visited(30000, false); 
+   queue<int>qu;
+   vector<int>path;
+   
+   qu.push(node);
+   visited[node]=true;
+
+   while(!qu.empty())
+   {
+     int cur = qu.front();
+     path.push_back(cur);
+     qu.pop();
+
+     for(int i:adj_list[cur])
+     {
+        if(!visited[i])
+        {
+            qu.push(i);
+            visited[i]=true;
+        }
+     }
+   }
+
+   for(auto it:path) cout<<it <<" ";
+
+}
+
 int main()
 {
-    int tt;
-    cin >> tt;
+   int n,m;
+   cout << "Enter the number of vertices & Edage : ";
+   cin >> n >> m;
 
-    while(tt--)
-    {
-        int n;
-        cin >> n;
-        string s,cop;
-        cin >> s;
-        cop = s;
+   while(m--)
+   {
+     int a,b;
+     cin>>a >>b;
 
-        string sub="";
-        vector<int>pos;
-        for(int i=n-1; i>=0; i--)
-        {
-           if(n-1==i)
-           {
-               sub += s[i];
-               pos.push_back(i);
-           }
-           else if(sub.back()<=s[i])
-           {
-              sub += s[i];
-              pos.push_back(i);
-           }
-        }
+     adj_list[a].push_back(b);
+     adj_list[b].push_back(a);
+   }
+   
+   int start=0;
+   BFS(start);
 
-        int cnt=0,i=sub.size()-1;
-        while(i>=0 && sub.back()==sub[i])
-        {
-            cnt++;
-            i--;
-        }
-
-        reverse(sub.begin(),sub.end());
-        for(int i=0; i<sub.size(); i++)
-        {
-            s[pos[i]]=sub[i];
-        }
-
-        sort(cop.begin(),cop.end());
-        if(cop==s)
-        cout << sub.size()-cnt <<endl;
-        else
-        cout <<"-1"<<endl;
-
-    }
 }
