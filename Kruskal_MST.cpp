6a2b5dfcddad5332a4730dc158be92985d0ec209
UthfolGhosh <uthfolghosh9038@gmail.com>
2024-12-02 05:50:18 +0600

    update kruskal MST code

--- Kruskal_MST.cpp
+++ Kruskal_MST.cpp
@@ -1,112 +1,73 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 #define endl '\n'
 #define ll long long
-#define faster_code ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define faster_code ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-struct node{
-    int par;
-    int rank;
-};
+const int N = 1e5 + 6;
+vector<int> parent(N);
+vector<int> sz(N);
 
-struct Edge{
-    int src;
-    int dst;
-    ll wt;
-};
-
-vector<node> dsuf;
-vector<Edge> mst;
-
-int find(int v){
-    if(dsuf[v].par==-1) return v;
-    return dsuf[v].par=find(dsuf[v].par);
+void make_set(int v) {
+    parent[v] = v;
+    sz[v] = 1;
 }
 
-void union_op(int frm,int to){
-    if(dsuf[frm].rank>dsuf[to].rank){
-        dsuf[to].par=frm;
-    }
-    else if(dsuf[frm].rank < dsuf[to].rank){
-        dsuf[frm].par=to; 
-    }
-    else{
-        dsuf[frm].par = to;
-        dsuf[to].rank +=1;
+int find_set(int v) {
+    if (v == parent[v]) return v;
+    return parent[v] = find_set(parent[v]); // Path compression
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+
+    if (a != b) {
+        if (sz[a] < sz[b]) swap(a, b);
+        parent[b] = a;
+        sz[a] += sz[b];
     }
 }
 
-bool comparator(Edge a,Edge b){
-    return a.wt < b.wt;
-}
-
-void Kruskals(vector<Edge> &edge, int v, int e){
-    sort(edge.begin(),edge.end(),comparator);
-    int i=0,j=0; 
-
-    while(i<v-1 && j<e) 
-    {
-        int frm_par = find(edge[j].src);
-        int to_par = find(edge[j].dst);
-
-        if(frm_par==to_par){
-            j++;
-            continue;
-        }
-
-        union_op(frm_par,to_par);
-        mst.push_back(edge[j]);
-        i++;
-        j++; 
-    }
-}
-
-void printnst(){
-    cout << "MST Path is : \n";
-    
-    ll cost = 0;
-    for(Edge x:mst )
-    {
-        cost += x.wt;
-
-        cout << x.src << " -> " << x.dst <<" = " << x.wt << endl;
-    }
-
-    cout << "Total cost of MST = " << cost <<endl; 
-}
-
-int main(){
+int main() {
     faster_code;
-    int e,v;
+
+    int v, e;
     cin >> v >> e;
 
-    dsuf.resize(v+1); 
-
-    for(int i=1;i<=v;++i) 
-    {
-        dsuf[i].par=-1;
-        dsuf[i].rank=0;
+    // Initialize disjoint sets
+    for (int i = 0; i < v; i++) {
+        make_set(i);
     }
 
-    vector<Edge> Eglist;
-    Edge temp;
-
-    for(int i=0; i<e; i++)
-    {
-        int frm,to,wt;
-        cin >> frm >> to >> wt;
-
-        temp.src = frm;
-        temp.dst = to;
-        temp.wt=wt;
-
-        Eglist.push_back(temp);
+    vector<vector<int>> edges;
+    for (int i = 0; i < e; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back({w, u, v});
     }
 
-    Kruskals(Eglist,v,e);
-    
-    printnst();
-    
+    sort(edges.begin(), edges.end()); // Sort edges by weight
+    int cost = 0;
+
+    for (auto it : edges) {
+        int w = it[0];
+        int u = it[1];
+        int v = it[2];
+
+        if (u == v) continue; // Exclude self-loops
+
+        int x = find_set(u);
+        int y = find_set(v);
+
+        if (x != y) {
+            cout << u << " " << v << endl;
+            cost += w;
+            union_sets(u, v);
+        }
+    }
+    cout << cost << endl;
+
     return 0;
 }
